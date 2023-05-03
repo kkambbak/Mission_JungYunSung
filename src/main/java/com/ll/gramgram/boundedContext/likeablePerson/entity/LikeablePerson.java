ecd593bd55b42baef24163bbb6287e391a05b075
kkambbak <yunsung9841@gmail.com>
2023-05-03 16:41:18 +0900

    호감표시/호감사유 변경후 3시간 쿨타임 적용

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Entity
@@ -40,7 +41,7 @@ public class LikeablePerson extends BaseEntity {
 
     // 초 단위에서 올림 해주세요.
     public String getModifyUnlockDateRemainStrHuman() {
-        return "2시간 16분";
+        return "%d시 %d분".formatted(modifyUnlockDate.getHour(), modifyUnlockDate.getMinute());
     }
 
     public RsData updateAttractionTypeCode(int attractiveTypeCode) {
