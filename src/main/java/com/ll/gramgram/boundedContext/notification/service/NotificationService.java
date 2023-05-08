79f7440aa27f32d48d14760856ff70f6bac813ec
kkambbak <yunsung9841@gmail.com>
2023-05-08 21:35:26 +0900

    호감 리스트

--- src/main/java/com/ll/gramgram/boundedContext/notification/service/NotificationService.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/service/NotificationService.java
@@ -18,7 +18,7 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
 
     public List<Notification> findByToInstaMember(InstaMember toInstaMember) {
-        return notificationRepository.findByToInstaMember(toInstaMember);
+        return notificationRepository.findByToInstaMemberOrderByIdDesc(toInstaMember);
     }
 
     @Transactional
@@ -47,7 +47,7 @@ public class NotificationService {
     }
 
     public List<Notification> findByToInstaMember_username(String username) {
-        return notificationRepository.findByToInstaMember_username(username);
+        return notificationRepository.findByToInstaMember_usernameOrderByIdDesc(username);
     }
 
     @Transactional
